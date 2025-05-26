3b2c13c202fe31895ba704ce6084937045374de5
semiyuri <juriyunna27@gmail.com>
2025-05-27 01:04:54 +0300

    feat: implement AuthForm component for sign-in and sign-up pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="SIGN_IN" />
+    </section>
+  );
 };
 
 export default SignIn;
